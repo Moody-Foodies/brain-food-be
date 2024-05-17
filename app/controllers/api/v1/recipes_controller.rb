8087cc78d2e66de0caa1bfbfc4d225a2eef0676d
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-17 14:02:38 -0400

    test: mood service

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::RecipesController < ApplicationController
     n = Nutrient.find_by(id: params[:mood])
     details = {:nutrient=> n, :cook_time=>params[:time_available]}
     recipes = RecipeFacade.new(details).get_recipe_data
-    
+    mood = { "user_id": params[:user_id].to_i, "mood": params[:mood].to_i }
+    mood = MoodFacade.new(mood).create_user_mood
 
     render json: RecipeSerializer.new(recipes).serializable_hash
   end
