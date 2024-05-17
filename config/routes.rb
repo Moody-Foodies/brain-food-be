ace279646181efbbd4d44c6d5ca56a20aede24d0
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-17 16:03:00 -0400

    Initial Setup for favorite recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'recipes', to: 'recipes#index'
+      namespace :recipes do
+        resources :favorites, only: [:index, :create, :destroy]
+      end
     end
   end
 end
