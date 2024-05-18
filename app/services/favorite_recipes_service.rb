ace0902104234d81eeadefdb9103136cc6e6f506
Jack Sweeney <jackcsweeney@gmail.com>
2024-05-18 12:18:04 -0700

    fix favorite recipes base url issue

--- app/services/favorite_recipes_service.rb
+++ app/services/favorite_recipes_service.rb
@@ -1,10 +1,10 @@
 class FavoriteRecipesService
   def conn
-    Faraday.new(url: "https://favorite-recipes-service-7d6cb7e82492.herokuapp.com/api/v1/favorite_recipes")
+    Faraday.new(url: "https://favorite-recipes-service-7d6cb7e82492.herokuapp.com")
   end
 
   def create_recipe(recipe_details)
-    response = conn.post("/api/v1/recipes/favorites") do |req|
+    response = conn.post("/api/v1/favorite_recipes") do |req|
       req.headers['Content-Type'] = 'application/json'
       req.body = recipe_details.to_json
     end
