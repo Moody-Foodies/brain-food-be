a9981141eb847158d79e14a69afb50764f911040
Jack Sweeney <jackcsweeney@gmail.com>
2024-05-17 17:26:48 -0700

    test: Fix test for retrieving recipes so CI can function by mock call to postman server rather than trying to actually connect

--- spec/requests/api/v1/recipes/post_recipes_spec.rb
+++ spec/requests/api/v1/recipes/post_recipes_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V1::RecipesController", type: :request do
   describe "POST /api/v1/recipes" do
     let(:mood) { 5 }
-    let(:time_available) { 45 }
+    let(:time_available) { 60 }
     let(:user_id) { 1 }
     let(:body) { { mood: mood, time_available: time_available, user_id: user_id } }
     let!(:nutrient) do
@@ -15,9 +15,26 @@ RSpec.describe "Api::V1::RecipesController", type: :request do
     end
 
     before do
-      WebMock.allow_net_connect!
+      stub_request(:post, "http://127.0.0.1:5000/api/v1/moods").
+         with(
+           body: "{\"user_id\":1,\"mood\":5}",
+           headers: {
+          'Accept'=>'application/json',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'User-Agent'=>'Faraday v2.9.0'
+           }).
+         to_return(status: 200, body: "", headers: {})
 
-      post "/api/v1/recipes", params: body
+      json_response = File.read("spec/fixtures/get_recipes_from_service.json")
+      stub_request(:get, "https://7a97657d-b4dd-468a-960b-563f46161622.mock.pstmn.io/recipes?cook_time=60&nutrient=Magnesium").
+        with(
+          headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.9.0'
+          }).
+        to_return(status: 200, body: json_response, headers: {})
     end
 
     after do
@@ -25,6 +42,8 @@ RSpec.describe "Api::V1::RecipesController", type: :request do
     end
 
     it "returns recipes with the expected format" do
+      post "/api/v1/recipes", params: body
+
       expect(response).to have_http_status(:success)
 
       json_response = JSON.parse(response.body)
