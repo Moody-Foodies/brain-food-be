274ac9182b24e412caa113bde50d0eb37f4bba09
Jack Sweeney <jackcsweeney@gmail.com>
2024-05-18 14:04:13 -0700

    remove errant pry

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,6 +1,5 @@
 class RecipeService
   def get_recipes(details)
-    require 'pry' ; binding.pry
     response = get_url("recipes?nutrient=#{details[:nutrient].name}&cook_time=#{details[:cook_time]}")
   end
 
