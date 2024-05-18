9eec858a9d18582502aaf7520ff5a851becafb5d
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-18 10:33:55 -0400

    test: service

--- spec/services/favorites_service_spec.rb
+++ spec/services/favorites_service_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe FavoritesService do
     @service = FavoritesService
     stub_request(:delete, "https://recipes-service-be-27616f8124c6.herokuapp.com/api/v1/favorite_recipes")
       .to_return(status: 202, body: JSON.generate({})) 
+
+    @get_service = FavoritesService
+    @json_response = File.read("spec/fixtures/get_favorite_recipes_request.json")
+    stub_request(:get, "https://recipes-service-be-27616f8124c6.herokuapp.com/api/v1/favorite_recipes?user_id=1")
+        .to_return(status: 200, body: @json_response)
   end
 
   describe '#initialize' do
@@ -25,6 +30,14 @@ RSpec.describe FavoritesService do
         result = @service.delete_url("/api/v1/favorite_recipes", {user_id: "1", id: "123"})
 
         expect(result.env[:method]).to eq(:delete)
+      end
+    end
+
+    describe '.get_url(url)' do
+      it 'will use the connection to make a delete request' do
+        result = @service.get_url("/api/v1/favorite_recipes?user_id=1")
+
+        expect(result).to eq(JSON.parse(@json_response, symbolize_names: true))
       end
     end
   end
