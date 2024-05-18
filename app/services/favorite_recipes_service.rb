fe86fcc27df0d247bdc0da3293f1f31b0532db13
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-18 11:37:53 -0400

    fix route favorite recipe service

--- app/services/favorite_recipes_service.rb
+++ app/services/favorite_recipes_service.rb
@@ -1,6 +1,6 @@
 class FavoriteRecipesService
   def conn
-    Faraday.new(url: "https://7a97657d-b4dd-468a-960b-563f46161622.mock.pstmn.io")
+    Faraday.new(url: "https://favorite-recipes-service-7d6cb7e82492.herokuapp.com/api/v1/favorite_recipes")
   end
 
   def create_recipe(recipe_details)
