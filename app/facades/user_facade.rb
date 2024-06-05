2f9de24c09483ab9aa58a257f8b70d3d4f6f2f68
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-06-05 15:35:43 -0400

    use of memoization to possibly reduce amount of times API and services are hit

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,14 +1,17 @@
 class UserFacade
   def initialize(user_id)
     @user_id = user_id
+    @dashboard = nil
   end
 
   def get_dashboard
-    recipes = FavoritesService.get_favorite_recipes(@user_id)
-    moods = MoodService.new.get_moods(@user_id)
-    {
-      recipes: recipes,
-      moods: moods
-    }
+    @dashboard ||= begin
+      recipes = FavoritesService.get_favorite_recipes(@user_id)
+      moods = MoodService.new.get_moods(@user_id)
+      @dashboard = {
+        recipes: recipes,
+        moods: moods
+      }
+    end
   end
 end
